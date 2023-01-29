60e659658bdbf92b99bdaf2a7ac986e0bdb01e04
Lily Russ <llyruss@yahoo.com>
2023-01-29 16:44:14 -0600

    banner

---
+++ react-portfolio/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+import React from "react";
+import Banner from "./components/Banner/Banner";
+
+function App() {
+    return (
+      
+     <Router>
+        <Banner />
+     </Router>
+  )
+
+}
+
+
+
+export default App;
