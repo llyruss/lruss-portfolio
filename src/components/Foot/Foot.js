487444ffe345bd22a50e297407b68eeca06766b8
Lily Russ <llyruss@yahoo.com>
2023-02-12 17:52:04 -0600

    css stuff

--- src/components/Foot/Foot.js
+++ src/components/Foot/Foot.js
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <footer>
     <div id="contact">
-        <h4>(603) 852 - 3714 | llyruss@yahoo.com</h4>
+        <h4 className = "foot">(603) 852 - 3714 | llyruss@yahoo.com</h4>
         <div id="icons">
        
             <a href="https://github.com/llyruss" target="_blank" rel="noreferrer"><i id="githubIcon" class="fab fa-github fa-3x"></i></a>
