6760e013d6906b13e950bbabd28d8d17e94faf71
Lily Russ <llyruss@yahoo.com>
2023-02-12 20:29:59 -0600

    updated nav, included redirect

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Nav from "./components/Nav/Nav"
 import Foot from "./components/Foot/Foot"
 import About from "./components/About/About"
 import Contact from "./components/Contact/contact"
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 
 import Skills from "./components/Skills/Skills"
 import Portfolio from "./components/Portfolio/Portfolio"
@@ -19,10 +19,11 @@ function App() {
       <Nav />
 
       <Routes>
-        <Route exact path ="/" element= {<About/>}></Route>
+        <Route exact path ="/react-portfolio/" element= {<About/>}></Route>
         <Route exact path ="/contact" element={<Contact/>}></Route>
         <Route path="/skills" element={<Skills />}></Route>
         <Route path="/portfolio" element={<Portfolio />}>
+        <Route path='*' element={<Navigate to='/react-portfolio'/>} />
         </Route>
 
       </Routes>
--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -7,7 +7,7 @@ function Nav() {
     <header>
         <nav>
             <ul>
-                <li><NavLink to="/">About</NavLink></li>
+                <li><NavLink to="/react-portfolio">About</NavLink></li>
                 <li><NavLink to="/skills">Skills</NavLink></li>
                 <li><NavLink to="/portfolio">Portfolio</NavLink></li>
                 <li><a href="https://docs.google.com/document/d/1K_5iYDtZQFixDea5ptGWI1-_p8hnUM_4SlY8Kou9CMI/edit?usp=sharing" target="_blank" rel="noreferrer">Resume</a></li>
